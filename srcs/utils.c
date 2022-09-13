39a944064258348aa5e06279a0ea7bacfdf54727
Jérémy <jeremy.barette@icloud.com>
2022-09-13 16:56:34 +0200

    Suppression des fonctions inutiles

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,26 +6,11 @@
 /*   By: jbarette <jbarette@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 00:34:31 by jbarette          #+#    #+#             */
-/*   Updated: 2022/09/13 14:22:29 by jbarette         ###   ########.fr       */
+/*   Updated: 2022/09/13 16:55:59 by jbarette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	free_array(char **array)
-{
-	int	i;
-
-	i = 0;
-	while (array[i])
-	{
-		free(array[i]);
-		array[i] = NULL;
-		i++;
-	}
-	free(array);
-	array = NULL;
-}
 
 char	**delete_element_tab(char **argv, int argc, int pos)
 {
